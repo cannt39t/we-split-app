20b8e5f1e7be3b5fad689a9b1d2663ebd31b5fe3
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-04-14 23:24:12 +0300

    Initial commit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Илья Казначеев on 14.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form{
+            Text("Hello, World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Илья Казначеев on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
